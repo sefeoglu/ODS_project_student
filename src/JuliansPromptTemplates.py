9a42e25da20357848a31d0d6dc31ee813613cda5
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2024-01-13 17:18:12 +0100

    prepared new prompt templates for the alignment cases #10

--- src/JuliansPromptTemplates.py
+++ src/JuliansPromptTemplates.py
@@ -1,15 +1,47 @@
 import json
 
 problemDefinition = 'In this task, we are given two ontologies in the form of <Subject> is <relation> of <Object>, which consist of classes and properties.'
-objective = 'Our objective is to provide ontology mapping for the provided ontologies based on their semantic similarities.'
+objective = 'Our objective is to provide ontology mapping for the provided ontologie based on their semantic similarities.'
 #importing data1
-def importTriples (json_file_path = 'triples_randomWalk_verbalized.json'):
+def importTriples(json_file_path = 'alignments.json'):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
-    global data1Keys
-    dataKeys = list(data.keys())
-    return dataKeys, data
+    return data
 
+def groupTriples():
+    data = importTriples()
+    tuples = []
+    for triple in data:
+        onto, node1 = triple[0].split('#')
+        onto, node2 = triple[1].split('#')
+        tuples.append((node1, node2))
+    return tuples
+
+#promptCounter is the index of the desired prompt, if promptCounter < 0 all prompts are returned as a list
+def JPrompt(promptCounter = -1):
+    tuples = groupTriples()
+    if (promptCounter < 0 or promptCounter >= len(tuples)):
+        prompt = []
+        for i in range(len(tuples)):
+            prompt.append(JPrompt(i))
+    else: 
+        prompt = f'Is the concept "{tuples[promptCounter][0]}" the same as the concept "{tuples[promptCounter][1]}" yes or no?'
+    return prompt
+
+def JPrompt2(promptCounter = -1):
+    tuples = groupTriples()
+    prompt = ''
+    if (promptCounter < 0 or promptCounter >= len(tuples)):
+        for i in range(len(tuples)):
+            prompt += JPrompt(i)
+            prompt += '\n----------------------------------------------\n'
+    else: 
+        prompt = 'I will give you a Tuple in the form of (Class/Object from Ontology1, Class/Object from Ontology2).\n\n'
+        prompt += f"({tuples[promptCounter][0]}, {tuples[promptCounter][1]})"
+        prompt += '\n\n' + objective
+    return prompt
+
+"""
 #prepare some prompts from GPT paper: https://disi.unitn.it/~pavel/om2023/papers/om2023_STpaper1.pdf
 def prompt1():
     data1Keys, data1 = importTriples()
@@ -40,5 +72,11 @@ def prompt7():
     return prompt1, prompt2
 
 #print(prompt1())
-print('\n'.join(prompt2()))
+#print('\n'.join(prompt2()))
 #print('\n'.join(prompt7()))
+"""
+
+
+p = JPrompt()
+#p = JPrompt(4)
+print('\n'.join(p) if (type(p) == type([])) else p)
