fd8b4f981148cf1e4b71c3eb77afc7b02c774555
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2024-02-28 23:47:05 +0100

    fixed bugs

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,15 @@
 import json
 from verbalizer.Prompt_generator2.graph2text.utils import *
 
-def saveToJson(List, path, messageText = 'saved'):
+def saveToJson(List, path, messageText = 'saved', doPrint = True):
     data = json.dumps(List, indent='\t')
+    dirPath = '/'.join(path.split('/')[:-1]) + '/'
+    if not os.path.exists(dirPath):
+        print('mkdir:', dirPath)
+        os.mkdir(dirPath)
     with open(path, 'w') as json_file:
         json_file.write(data)
-    print(f"{messageText} {path}")
+    if doPrint: print(f"{messageText} {path}")
 
 def importFromJson(path):
     with open(path, 'r') as json_file:
